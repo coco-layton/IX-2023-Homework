5098aabc01e6cba40125dd1fa1851e0283b2cfe8
coco-layton <coco13@stanford.edu>
2023-07-17 18:11:55 +0100

    finish task list hw

--- week_1/day5/main.js
+++ week_1/day5/main.js
@@ -1,10 +1,13 @@
 console.log('Connected!');
 
 // Classes
-
 class Task {
   constructor(taskName) {
     this.taskName = taskName;
+  }
+
+  static fromJSON(json) {
+    return new Book(json.title, json.author, json.isbn);
   }
 }
 
@@ -19,8 +22,8 @@ class UI {
   
       this.form.addEventListener('submit', (e) => this.onFormSubmit(e));
   
-      this.tasks = [];
-  
+      this.taskList = [];
+      this.loadTaskListFromLocalStorage();
       this.renderTaskList();
     }
   
@@ -33,7 +36,9 @@ class UI {
       }
   
       const task = new Task(this.taskName.value);
-      this.tasks.push(task);
+      this.taskList.push(task);
+
+      this.saveTaskListToLocalStorage();
       this.renderTaskList();
   
       this.taskName.value = ''; // reset input to be blank
@@ -42,8 +47,8 @@ class UI {
     renderTaskList() {
       this.tableBody.innerHTML = '';
   
-      for (let i = 0; i < this.tasks.length; i++) {
-        const task = this.tasks[i];
+      for (let i = 0; i < this.taskList.length; i++) {
+        const task = this.taskList[i];
   
         const tr = this.createTaskRow(task);
         this.tableBody.appendChild(tr);
@@ -62,19 +67,18 @@ class UI {
       // add complete button
       const completeButton = document.createElement('input');
       completeButton.setAttribute('type', 'radio');
-      //completeButton.setAttribute('class', 'form-check-input');
       completeButton.addEventListener('click', () => {
         console.log('Complete was clicked');
       });
       tdComplete.appendChild(completeButton);
 
     
-      // add action buttons
+      // add delete buttons
       const deleteButton = document.createElement('button');
       deleteButton.setAttribute('class', 'btn btn-danger btn-sm me-1');
       deleteButton.innerHTML = 'Delete';
       deleteButton.addEventListener('click', () => {
-        console.log('Delete was clicked');
+        this.onDeleteTaskClicked(task);
       });
       tdActions.appendChild(deleteButton);
   
@@ -83,6 +87,31 @@ class UI {
       tr.appendChild(tdActions);
   
       return tr;
+    }
+
+    onDeleteTaskClicked(task) {
+        this.filterTaskListArray(task); //????
+        this.saveTaskListToLocalStorage();
+        this.renderTaskList();
+    }
+
+    filterTaskListArray(task) {
+        this.taskList = this.taskList.filter((currentTask) => {
+          return task.taskName != currentTask.taskName;
+        });
+    }
+
+    saveTaskListToLocalStorage() {
+        const json = JSON.stringify(this.taskList);
+        localStorage.setItem('taskList', json);
+    }
+
+    loadTaskListFromLocalStorage() {
+        const json = localStorage.getItem('taskList');
+        if (json) {
+          const taskListArr = JSON.parse(json);
+          this.taskList = taskListArr.map((task) => Task.fromJSON(task));
+        }
     }
   }
   
